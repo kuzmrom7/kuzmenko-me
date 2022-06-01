416b33ee7b08a2562eaaa7ebaf4efebf78f089df
kuzmrom7 <kuzmrom7@gmail.com>
2022-06-02 00:00:30 +0500

    update meta

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -3,14 +3,22 @@ import { MDXRemote } from 'next-mdx-remote';
 import type { InferGetStaticPropsType } from 'next';
 
 import styles from '../styles/cv.module.css';
+import Head from "next/head";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function CV({ source }: Props) {
   return (
-    <main className={styles.main}>
-      <MDXRemote {...source} />
-    </main>
+    <>
+      <Head>
+        <title>CV Roman Kuzmenko</title>
+        <meta name="description" content="Roman Kuzmenko-Frontend-developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <MDXRemote {...source} />
+      </main>
+    </>
   );
 }
 
