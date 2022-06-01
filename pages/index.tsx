fdeebd3349c5c2b4a7d72fbb590a487909625c25
kuzmrom7 <kuzmrom7@gmail.com>
2022-06-01 13:25:15 +0500

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,16 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Roman Kuzmenko</title>
         <meta name="description" content="Roman Kuzmenko-Frontend-developer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        {/*<header className={styles.header}></header>*/}
         <h1 className={styles.title}>Roman Kuzmenko</h1>
-        <p className={styles.description}>Frontend developer</p>
+        <p className={styles.description}>Frontend developer from Saint-Petersburg</p>
         <footer className={styles.footer}>
           <div className={styles.link}>
             <a href="https://github.com/Kuzmrom7">Github</a>
@@ -20,9 +21,12 @@ const Home: NextPage = () => {
           <div className={styles.link}>
             <a href="https://t.me/rkuzmen">Telegram</a>
           </div>
+          <div className={styles.link}>
+            <a href="https://romakuzmenko.notion.site/1de0f979faa54c7295652de386e03716">CV</a>
+          </div>
         </footer>
       </main>
-    </div>
+    </>
   )
 }
 
